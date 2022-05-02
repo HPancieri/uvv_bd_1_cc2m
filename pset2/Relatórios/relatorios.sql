3ad0b74c3d45d6c0a378c31a689dcf62fe59e131
HPancieri <pancieri.trabalhos@gmail.com>
2022-05-02 17:16:31 -0300

    Resolvidas as questões 5 - 10 do PSet 2.

--- pset2/Relatórios/relatorios.sql
+++ pset2/Relatórios/relatorios.sql
@@ -18,8 +18,8 @@ FROM
 	funcionario AS f
 	INNER JOIN
 	departamento AS d
-ON
-	(f.numero_departamento = d.numero_departamento)
+	ON
+		(f.numero_departamento = d.numero_departamento)
 GROUP BY
 	d.numero_departamento, f.numero_departamento
 ORDER BY
@@ -94,3 +94,154 @@ ORDER BY
 	"Nome do Funcionário" ASC;
 
 ----------------------------------------//----------------------------------------
+
+-- QUESTÃO 05: prepare um relatório que liste, para cada departamento, o nome
+-- do gerente e o nome dos funcionários. Ordene esse relatório por nome do departamento
+-- (em ordem crescente) e por salário dos funcionários (em ordem decrescente).
+
+SELECT
+	gerentes.nome_departamento AS "Nome do Departamento",
+	CONCAT(gerentes.primeiro_nome, ' ', gerentes.nome_meio, '. ', gerentes.ultimo_nome) AS "Nome do Gerente",
+	CONCAT(funcionarios.primeiro_nome, ' ', funcionarios.nome_meio, '. ', funcionarios.ultimo_nome) AS "Nome do Funcionario"
+FROM
+	(
+		departamento
+		INNER JOIN
+		funcionario
+		ON
+			departamento.cpf_gerente = funcionario.cpf
+	) AS gerentes,
+	(
+		departamento
+		INNER JOIN
+		funcionario
+		ON
+			departamento.numero_departamento = funcionario.numero_departamento
+	) AS funcionarios
+ORDER BY
+	gerentes.nome_departamento ASC,
+	funcionarios.salario DESC;
+
+----------------------------------------//----------------------------------------
+
+-- QUESTÃO 06: prepare um relatório que mostre o nome completo dos funcionários
+-- que têm dependentes, o departamento onde eles trabalham e, para cada funcionário,
+-- também liste o nome completo dos dependentes, a idade em anos de cada
+-- dependente e o sexo (o sexo NÃO DEVE aparecer como M ou F, deve aparecer
+-- como “Masculino” ou “Feminino”).
+
+SELECT
+	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
+	d.nome_departamento AS "Nome do Departamento",
+	dep.nome_dependente AS "Nome do Dependente",
+	DATE_PART('year', AGE(CURRENT_DATE, dep.data_nascimento)) AS "Idade do Dependente",
+	CASE dep.sexo
+		WHEN 'M' THEN 'Masculino'
+		WHEN 'F' THEN 'Feminino'
+	END AS "Sexo do Dependente"
+FROM
+	funcionario AS F
+	INNER JOIN
+	departamento AS d
+	ON
+		f.numero_departamento = d.numero_departamento
+	INNER JOIN
+	dependente AS dep
+	ON
+		f.cpf = dep.cpf_funcionario
+ORDER BY
+	"Idade do Dependente" ASC;
+
+----------------------------------------//----------------------------------------
+
+-- QUESTÃO 07: prepare um relatório que mostre, para cada funcionário que NÃO
+-- TEM dependente, seu nome completo, departamento e salário.
+
+SELECT
+	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
+	d.nome_departamento AS "Nome do Departamento",
+	CAST(f.salario AS MONEY) AS "Salário"
+FROM
+	funcionario AS f
+	INNER JOIN
+	departamento AS d
+	ON
+		f.numero_departamento = d.numero_departamento
+WHERE NOT EXISTS
+	(
+		SELECT
+			*
+		FROM
+			dependente AS dep
+		WHERE
+			f.cpf = dep.cpf_funcionario
+	)
+ORDER BY
+	"Nome do Funcionário" ASC;
+
+----------------------------------------//----------------------------------------
+
+-- QUESTÃO 08: prepare um relatório que mostre, para cada departamento, os projetos
+-- desse departamento e o nome completo dos funcionários que estão alocados
+-- em cada projeto. Além disso inclua o número de horas trabalhadas por cada funcionário,
+-- em cada projeto.
+
+SELECT
+	d.nome_departamento AS "Nome do Departamento",
+	p.nome_projeto AS "Nome do Projeto",
+	CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS "Nome do Funcionário",
+	te.horas AS "Horas Trabalhadas"
+FROM
+	departamento AS d
+	NATURAL JOIN
+	projeto AS p
+	NATURAL JOIN
+	trabalha_em AS te
+	INNER JOIN
+	funcionario AS f
+	ON
+		f.cpf = te.cpf_funcionario
+ORDER BY
+	d.nome_departamento ASC,
+	p.nome_projeto ASC,
+	"Nome do Funcionário" ASC;
+
+----------------------------------------//----------------------------------------
+
+-- QUESTÃO 09: prepare um relatório que mostre a soma total das horas de cada
+-- projeto em cada departamento. Obs: o relatório deve exibir o nome do
+-- departamento, o nome do projeto e a soma total das horas.
+
+SELECT
+	d.nome_departamento AS "Nome do Departamento",
+	p.nome_projeto AS "Nome do Projeto",
+	SUM(te.horas) AS "Total de Horas Trabalhadas"
+FROM
+	trabalha_em AS te
+	NATURAL JOIN
+	projeto AS p
+	NATURAL JOIN
+	departamento AS d
+GROUP BY
+	d.nome_departamento,
+	p.nome_projeto
+ORDER BY
+	d.nome_departamento ASC,
+	p.nome_projeto ASC;
+
+----------------------------------------//----------------------------------------
+
+-- QUESTÃO 10: prepare um relatório que mostre a média salarial dos
+-- funcionários de cada departamento.
+
+SELECT
+	d.nome_departamento AS "Nome do Departamento",
+	CAST(AVG(f.salario) AS MONEY) AS "Média Salarial"
+FROM
+	departamento AS d
+	NATURAL JOIN
+	funcionario AS f
+GROUP BY
+	d.nome_departamento
+ORDER BY
+	d.nome_departamento ASC;
